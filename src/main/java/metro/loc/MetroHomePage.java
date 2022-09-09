4ca4109f496838660b80f85805ad6738d68260b7
Andrey Trusov <abt924@gmail.com>
2022-09-09 21:59:04 +0300

    home page added

---
+++ src/main/java/metro/loc/MetroHomePage.java
@@ -0,0 +1,111 @@
+package metro.loc;
+
+import com.codeborne.selenide.ElementsCollection;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+
+public class MetroHomePage {
+    // поле с экземпляром веб драйвера
+    private final WebDriver driver;
+
+    // локатор кнопки выпадающего списка городов по имени класса
+    private final By selectCityButton = By.className("select_metro__button");
+
+    // локатор поля ввода «Откуда» по XPATH, поиск по плейсхолдеру
+    private final By fieldFrom = By.xpath(".//input[contains(@placeholder, 'Откуда')]");
+
+    // локатор поля ввода «Куда» по XPATH, поиск по плейсхолдеру
+    private final By fieldTo = By.xpath(".//input[contains(@placeholder, 'Куда')]");
+
+    // локатор коллекций станций «Откуда» и «Куда» маршрута по имени класса
+    private final By routeCollection = By.className("route-list _init _live-events");
+
+    private ElementsCollection routeStationFromTo;
+
+    // конструктор класса MetroHomePage с нужным параметром
+    public MetroHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // метод ожидания загрузки страницы: проверили видимость станции «Театральная»
+    public void waitForLoadHomePage() {
+        // подожди 8 секунд, пока появится веб-элемент с нужным текстом
+        new WebDriverWait(driver, Duration.ofSeconds(8) ).until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Театральная")));
+    }
+
+    // метод выбора города по переданному названию
+    public void chooseCity(String cityName) {
+        // кликни по выпадающему списку городов
+        driver.findElement(selectCityButton).click();
+        // выбери город, переданный в параметре метода
+        String xpath = ".//span[text()='" + cityName + "']";
+        driver.findElement(By.xpath(xpath)).click();
+    }
+
+    // метод ввода названия станции в поле «Откуда»
+    public void setStationFrom(String station) {
+        // введи название станции в поле ввода, а затем с помощью клавиш «Вниз» и Enter выбери его в выпадающем списке саджеста
+        driver.findElement(fieldFrom).sendKeys(station, Keys.DOWN, Keys.ENTER);
+    }
+
+    // метод ввода названия станции в поле «Куда»
+    public void setStationTo(String station) {
+        // введи название станции в поле ввода, а затем с помощью клавиш «Вниз» и Enter выбери его в выпадающем списке саджеста
+        driver.findElement(fieldTo).sendKeys(station, Keys.DOWN, Keys.ENTER);
+
+    }
+
+    // метод ожидания построения маршрута: проверяется видимость кнопки «Получить ссылку на маршрут»
+    public void waitForLoadRoute() {
+        // подожди 3 секунды, чтобы элемент с нужным текстом стал видимым
+        new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//span[contains(text(), 'Получить ссылку')]")));
+    }
+
+    // метод построения маршрута
+    public void buildRoute(String fromStation, String toStation) {
+        // указание станции «Откуда»
+        setStationFrom(fromStation);
+        // указание станции «Куда»
+        setStationTo(toStation);
+        // ожидание построения маршрута
+        waitForLoadRoute();
+        routeStationFromTo = $$(By.className("route-details-block__station-list-item"));
+    }
+
+    // метод получения имени станции «Откуда» для построенного маршрута
+    public String getRouteStationFrom() {
+        // возвращается текст первого элемента коллекции — станции «Откуда» и «Куда»
+        return routeStationFromTo.first().$(By.className("route-details-block__terminal-station")).getText();
+    }
+
+    // метод получения имени станции «Куда» построенного маршрута
+    public String getRouteStationTo() {
+        // возвращается текст второго элемента коллекции — станции «Откуда» и «Куда»
+        return routeStationFromTo.last().$(By.className("route-details-block__terminal-station")).getText();
+    }
+
+    // метод получения примерного времени маршрута
+    public String getApproximateRouteTime(int routeNumber) {
+        // возвращается текст из требуемого по номеру элемента из коллекции времен всех маршрутов
+        return driver.findElements(routeCollection).get(routeNumber)
+                .findElement(By.className("route-list-item__time")).getText();
+    }
+
+    // метод проверки с ожиданием видимости станции метро
+    public void waitForStationVisibility(String stationName) {
+        // ждем видимости элемента с нужным текстом из параметра в течение 8 секунд
+
+        new WebDriverWait(driver, Duration.ofSeconds(8))
+            .until(ExpectedConditions.visibilityOfElementLocated(By.linkText(stationName)));
+
+    }
+}
